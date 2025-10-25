ae0886c0ee9a7542581f11683e98e8aa383c4f3e
Hipocrates <hipocrates1555@gmail.com>
2025-10-24 21:44:49 -0300

    Estilizando a sidebar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,10 +1,22 @@
+import Avatar from "../../components/avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/titulo"
+import { Descricao, BotaoTema, SideBarContainer } from "./styles"
 
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/Pocati.png" alt="" />
-    <Titulo fontSize={20}>Hipócrates Matheus</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Hipócrates Matheus</Titulo>
+      <Paragrafo tipo="secundario">
+        Pocati
+      </Paragrafo>
+      <Descricao tipo="principal">
+        Desenvolvedor Front-End Jr.
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
